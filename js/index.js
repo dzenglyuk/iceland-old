fda3be69cb8650400757a0b69e1d66677b53bdf1
Dzenhliuk Oleh <dzenglyuk23@ukr.net>
2018-05-13 10:44:36 +0000

    Initial

--- js/index.js
+++ js/index.js
@@ -17,4 +17,36 @@ $(function() {
         return false;
     });
 //Smooth Scrolling Ends
+//Excursion Photos Showing
+   $(".link1").click(function(){
+        $(".item").hide();
+        $(".photo1").show();
+        $(".excursions-list li").css('color', '#848FA1');
+        $(".link1").css('color', 'white');
+    });
+    $(".link2").click(function(){
+        $(".item").hide();
+        $(".photo2").show();
+        $(".excursions-list li").css('color', '#848FA1');
+        $(".link2").css('color', 'white');
+    });
+    $(".link3").click(function(){
+        $(".item").hide();
+        $(".photo3").show();
+        $(".excursions-list li").css('color', '#848FA1');
+        $(".link3").css('color', 'white');
+    });
+    $(".link4").click(function(){
+        $(".item").hide();
+        $(".photo4").show();
+        $(".excursions-list li").css('color', '#848FA1');
+        $(".link4").css('color', 'white');
+    });
+    $(".link5").click(function(){
+        $(".item").hide();
+        $(".photo5").show();
+        $(".excursions-list li").css('color', '#848FA1');
+        $(".link5").css('color', 'white');
+    });
+//Excursion Photos Showing Ends
 });
